a5d5f6631e858b4a7a7752d321f69131cc363555
LO1ZB <franzdervierte@web.de>
2014-04-05 14:01:02 +0200

    merge /burn from @tonibm19 into the plugin

--- Essentials/es_main.lua
+++ Essentials/es_main.lua
@@ -9,10 +9,11 @@ function Initialize(Plugin)
 	PLUGIN = Plugin -- NOTE: only needed if you want OnDisable() to use GetName() or something like that
 	
 	-- Command Bindings
-	cPluginManager.BindCommand("/heal", "es.heal", EsHandleHealCommand, " ~ Heal a player")
+ +	cPluginManager.BindCommand("/burn", "es.burn", EsHandleBurnCommand, " ~ burns a player")
 	cPluginManager.BindCommand("/fly", "es.fly", EsHandleFlyCommand, " ~ allows/disallow a player to fly")
 	cPluginManager.BindCommand("/feed", "es.feed", EsHandleFeedCommand, " ~ Feed a player")
 	cPluginManager.BindCommand("/flyspeed", "es.speed.fly", EsHandleFlyspeedCommand, " ~ allows/disallow a player to fly")
+	cPluginManager.BindCommand("/heal", "es.heal", EsHandleHealCommand, " ~ Heal a player")
     
 	LOG("Initialised " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
 	return true
