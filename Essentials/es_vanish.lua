612bc61f0afca5ac897335135b0890d4075b59f6
tonibm19 <tonibm19@gmail.com>
2014-04-19 20:36:44 +0200

    Some improvements [SEE DESC]
    -Fixed other players vanish.
    -Added a es.vanish.other permission.

--- Essentials/es_vanish.lua
+++ Essentials/es_vanish.lua
@@ -1,5 +1,5 @@
 function EsHandleVanishCommand( Split, Player )
- 	if (Split[3] == nil) then
+ 	if (Split[2] == nil) then
  	    if Player:IsVisible() == true then
             Player:SetVisible(false)
             Player:SendMessageInfo("You're now invisible!")
@@ -11,24 +11,30 @@ function EsHandleVanishCommand( Split, Player )
  	end
  	local IsVisible = false;
  	local VanishPlayer = function(OtherPlayer)
- 		if (OtherPlayer:GetName() == Split[2]) then
+ 		if (OtherPlayer:GetName() == Split[2]) and (OtherPlayer:GetName() ~= Player:GetName()) then
             if OtherPlayer:IsVisible() == true then
                 OtherPlayer:SetVisible(false)
                 OtherPlayer:SendMessageInfo("You're now invisible!")
+                IsVisible = 0
             else
                 OtherPlayer:SetVisible(true)
                 OtherPlayer:SendMessageInfo("You're now visible!")
+                IsVisible = 1
             end
-            IsVisible = true
             return true
  	    end
  	end
- 	cRoot:Get():FindAndDoWithPlayer(Split[2], VanishPlayer);
- 	if (IsVisible) then
- 		Player:SendMessageSuccess( "Player " .. Split[2] .. " is invisible (or visible if was already invisible)" )
- 		return true
- 	else
-		Player:SendMessageFailure( "Player not found" )
-		return true
-	end
+    if(Player:HasPermission("es.vanish.other")) then
+        cRoot:Get():FindAndDoWithPlayer(Split[2], VanishPlayer);
+        if (IsVisible == 0) then
+            Player:SendMessageSuccess( "Player " .. Split[2] .. " is now invisible" )
+            return true
+        elseif (IsVisible == 1) then
+            Player:SendMessageSuccess( "Player " .. Split[2] .. " is now visible" )
+            return true        
+        else
+            Player:SendMessageFailure( "Player not found" )
+            return true
+        end
+    end
 end
