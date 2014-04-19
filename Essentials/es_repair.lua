b8a600627572905d2bffecf4a8cd3c0fce6dcf4c
tonibm19 <tonibm19@gmail.com>
2014-04-19 20:51:53 +0200

    Repair is now working

--- Essentials/es_repair.lua
+++ Essentials/es_repair.lua
@@ -1,11 +1,12 @@
 function EsHandleRepairCommand(Split, Player)
         HoldedItem = Player:GetEquippedItem()
     if(HoldedItem:IsDamageable()) then
-        Item:DamageItem(HoldedItem, (HoldedItem.m_ItemDamage - HoldedItem:GetMaxDamage()))
-        Player:SendMessageSuccess("item repaired")
+        HoldedItem.m_ItemDamage = 0
+        Player:GetInventory():SetHotbarSlot(Player:GetInventory():GetEquippedSlotNum(), HoldedItem)
+        Player:SendMessageSuccess("Item repaired")
         return true
     else
-        Player:SendMessageFailure("please hold an item to repair")
+        Player:SendMessageFailure("Please hold an item to repair")
         return true
     end
     return true
